81548ae6074b663c14f9b8fb6ba212649eb1c33b
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-15 17:37:32 +0100

    Added routes to battle file

--- lib/battle.rb
+++ lib/battle.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
+  "Hello"
+end
+
+get '/secret' do
+  "You've reached a secret webpage"
+end
+
+get '/shotgun' do
+  "The routes have updated"
+end
+
+get '/latest' do
+  "The routes have updated again"
 end
