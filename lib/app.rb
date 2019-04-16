a3565d4a8fd2299d67661ca6707e86dddcd8bc92
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-16 12:40:14 +0100

    Added conditional rendering to index html

--- lib/app.rb
+++ lib/app.rb
@@ -4,7 +4,13 @@ get '/' do
   "Hello"
 end
 
-get '/cat' do
-  @random_name = ["Amigo","Oscar","Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo","Oscar","Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
